d4e6cfb2af3456d8db3f4f183a231170bf5b6e9a
Carlos Ortiz <carlosor@gmail.com>
2022-04-04 16:37:49 +0200

    wip

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -17,9 +17,9 @@ function getFolderName(str) {
 }
 
 function bytesToSize(bytes) {
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+   let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
    if (bytes == 0) return '0 Byte';
-   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+   let i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
 
